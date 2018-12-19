25bdcd23f7806ad9b39f6d2104cfc44da4dd5be8
KoffeinFlummi <koffeinflummi@gmail.com>
2018-12-19 13:56:09 +0100

    Add library to package

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate serde_derive;
+extern crate docopt;
+extern crate colored;
+extern crate byteorder;
+extern crate time;
+extern crate linked_hash_map;
+extern crate openssl;
+extern crate regex;
+
+mod armake;
+
+pub use armake::{pbo,config,p3d,preprocess,binarize,sign,io};
